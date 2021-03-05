1c9112903424ebbb30cd4c7dc49008ee486a16ff
Snowpack Build <actions@github.com>
2021-03-05 08:28:10 +0000

    Commit from GitHub Actions (Snowpack)

--- docs/dist/App.js
+++ docs/dist/App.js
@@ -10,7 +10,16 @@ class App extends React.Component {
       className: "container"
     }, /* @__PURE__ */ React.createElement("h1", {
       className: "text-center heading"
-    }, "MAFS Rater", /* @__PURE__ */ React.createElement("small", null, ".com")), /* @__PURE__ */ React.createElement(Rater, null));
+    }, "MAFS Rater", /* @__PURE__ */ React.createElement("small", null, ".com")), /* @__PURE__ */ React.createElement(Rater, null), /* @__PURE__ */ React.createElement("p", {
+      className: "footer"
+    }, "Made with ", /* @__PURE__ */ React.createElement("img", {
+      width: "16",
+      src: "images/heart-solid.svg",
+      alt: "love"
+    }), " by ", /* @__PURE__ */ React.createElement("a", {
+      href: "https://github.com/benkaiser",
+      target: "_blank"
+    }, "Benjamin Kaiser")));
   }
 }
 export default App;
